27c7eedbd66526996a14a5c845fdb5a532b1a6cd
Leyla <cydeo.leyla@gmail.com>
2022-08-21 15:43:12 -0400

    Java file committed then edited. Then added

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         // I did small changes
+        System.out.println("day1");
     }
 }
